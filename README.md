41cf71f63c88732209859a9490227d878d7149d0
obatsis <batsisorestis@gmail.com>
2021-03-06 09:46:57 +0200

    scripts update and setup instructions

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Distributed Systems Course 2020-2021 ToyChord Project
 
 
 ## Before you start
-Run `./dependencies.sh` to install dependencies.
+Read `virtual_env_help.md` and after creating your virtual environment, activate it
+Then run `./dependencies.sh` with root privileges to install all dependencies.
---
+++ dependencies.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# quick alias for python
+
+FILE=~/.bash_aliases
+if [ -f "$FILE" ]; then
+    echo "$FILE already exists."
+		if [[ `tail -n 1 $FILE` == "alias pip='pip3'" ]] ; then
+			echo " -------- aliases already in place!";
+		else
+				echo " -------- adding needed aliases..."
+				echo -e "\n#python aliases \nalias python='python3' \nalias pip='pip3'" >> $FILE
+		fi
+else
+    echo "$FILE does not exist."
+		touch ~/.bash_aliases && echo "creating file $FILE"
+		echo " -------- adding needed aliases..."
+		echo -e "\n#python aliases \nalias python='python3' \nalias pip='pip3'" >> $FILE
+fi
+
+
+# -e is for appending lines
+echo " -------- installing dependecies -------- "
+apt-get install python3-venv
+pip3 install Flask
+pip3 install requests
+pip3 install PyInquirer
