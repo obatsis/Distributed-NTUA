3cccfa61b61aa121511e73f7e4585b54d908d808
obatsis <batsisorestis@gmail.com>
2021-03-16 21:49:44 +0200

    .

--- auto/run_vms.sh
+++ auto/run_vms.sh
@@ -10,5 +10,5 @@ fi
 for node in master node1 node2 node3 node4
 do
 	echo -e '\033[1;91m  Inside node:\033[00m' $node
-	ssh user@$node '/home/user/Distributed-NTUA/auto/server_vm.sh $1 $2 && exit'
+	ssh user@$node "/home/user/Distributed-NTUA/auto/server_vm.sh $1 $2 && exit"
 done
