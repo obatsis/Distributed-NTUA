bc5e49d85bfdd7a381a43fd6e2ec4a0a601e8bbc
obatsis <batsisorestis@gmail.com>
2021-03-16 21:36:46 +0200

    .

--- auto/server_vm.sh
+++ auto/server_vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 hs=$HOSTNAME
 
-if [ $hs = "T460" ]; then
+if [ $hs = "master" ]; then
 	gnome-terminal --tab -- bash -c "python3 ~/Distributed-NTUA/server.py -p 5000 -k $1 -c $2 -b; exec bash"
 else
 	gnome-terminal --tab -- bash -c "python3 ~/Distributed-NTUA/server.py -p 5000 -k $1 -c $2; exec bash"
